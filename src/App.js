9fea05171c6491f76fe4d4a8572052d7a5011b12
Case Wylie <cwylie@redhat.com>
2020-11-20 18:20:37 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const styles = {
+      display: 'flex',
+      height: '100%vh !important',
+      position: 'absolute',
+      top:0,
+      bottom:0,
+      left:0,
+      right:0,
+      width:"100%",
+      backgroundColor:process.env.REACT_APP_THEME=="light"?"white":"black"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+     asdfasdf
     </div>
   );
 }
