d1ea807954085a02339762c1577d2a8d241c0e5e
Case Wylie <cwylie@redhat.com>
2020-11-20 20:40:40 -0500

    make unit test fail

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/asdfasd/i);
+  const linkElement = screen.getByText(/asdfasdx/i);
   expect(linkElement).toBeInTheDocument();
 });
