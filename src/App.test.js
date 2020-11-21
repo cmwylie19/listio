a32e2f9b8aef14bc17c260c58dd8df7d9dd49392
Case Wylie <cwylie@redhat.com>
2020-11-20 20:36:39 -0500

    unit test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/asdfasdf/i);
+  const linkElement = screen.getByText(/asdfasd/i);
   expect(linkElement).toBeInTheDocument();
 });
